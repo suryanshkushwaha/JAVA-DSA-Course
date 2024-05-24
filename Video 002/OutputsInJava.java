34a4a47dafb522eb04d3dd91dc0f0ddd2c56d244
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-05-25 00:47:44 +0800

    Video 002 - Output in Java

---
+++ Video 002/OutputsInJava.java
@@ -0,0 +1,10 @@
+public class OutputsInJava {
+    public static void main(String[] args) {
+        System.out.print("Hello"); // cursor stays on the same line
+        System.out.println("Hello"); // cursor goes onto the next line
+        System.out.print("Hello\n");  // escape sequence cursor goes to new line
+
+        System.out.println("1 2 3 4 5");
+    }
+}
+// boilerplate code
